8f4f1d2e9aa52c9835a63d9348520f97947f54b6
edu292 <eduskoroboatei@gmail.com>
2025-12-18 20:51:06 -0400

    WIP: episode selector for series entries

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-from collections.abc import Container
 from io import BytesIO
-import asyncio
+
+from httpx import AsyncClient
 from textual.app import App, ComposeResult
-from textual.screen import Screen
 from textual.containers import (
-    HorizontalScroll,
     Center,
-    Vertical,
     Horizontal,
+    HorizontalScroll,
+    Vertical,
     VerticalScroll,
 )
-from textual.widgets import Button, Footer, Input, Label
 from textual.message import Message
+from textual.reactive import reactive
+from textual.screen import Screen
+from textual.widgets import Button, ContentSwitcher, Footer, Input, Label, Select
 from textual_image.widget import Image
-from httpx import AsyncClient
 
 from streaming import (
+    close_session,
     get_available_streams,
+    get_metadata,
     get_session_handle,
     search_catalog,
-    get_metadata,
-    close_session,
     start_download,
 )
 
@@ -33,8 +33,21 @@ async def fetch_url(url):
         return BytesIO(response.content)
 
 
+class UrlImage(Image):
+    def __init__(self, url, **kwargs):
+        super().__init__(image=None, **kwargs)
+        self.url = url
+
+    async def on_mount(self):
+        response = await fetch_url(self.url)
+        if not response:
+            return
+
+        self.image = response
+
+
 class Poster(Vertical):
-    BINDINGS = [("enter", "select")]
+    BINDINGS = [('enter', 'select')]
     can_focus = True
 
     class Selected(Message):
@@ -47,7 +60,7 @@ class Poster(Vertical):
         super().__init__(**kwargs)
 
     async def on_mount(self) -> None:
-        response = await fetch_url(self.data["poster"])
+        response = await fetch_url(self.data['poster'])
         if not response:
             return
         await self.remove_children()
@@ -60,75 +73,157 @@ class Poster(Vertical):
         self.post_message(self.Selected(self.data))
 
     def compose(self) -> ComposeResult:
-        yield Label(self.data["name"])
+        yield Label(self.data['name'])
 
 
 class PosterList(HorizontalScroll):
-    def update_posters(self, posters_data):
+    posters_data = reactive(list)
+
+    def __init__(self, posters_data=None, **kwargs) -> None:
+        super().__init__(**kwargs)
+        if posters_data:
+            self.posters_data = posters_data
+
+    def watch_posters_data(self, posters_data):
         self.remove_children()
         self.scroll_home()
         new_posters = [Poster(data) for data in posters_data]
         self.mount_all(new_posters)
 
 
+class EpisodeCard(Horizontal):
+    can_focus = True
+
+    class Submitted(Message):
+        def __init__(self, season_id, episode_id) -> None:
+            super().__init__()
+
+    def __init__(self, season_id, episode_data, **kwargs):
+        super().__init__(**kwargs)
+        self.season_id = season_id
+        self.episode_data = episode_data
+
+    def on_click(self):
+        self.post_message(self.Submitted(self.season_id, self.episode_id))
+
+    def compose(self) -> ComposeResult:
+        yield UrlImage(self.episode_data['thumbnail'])
+        with Horizontal(classes='episode-details'):
+            yield Label(self.episode_data['name'], classes='episode-name')
+            yield Label(self.episode_data['released'], classes='episode-release')
+
+
+class EpisodeSelector(Vertical):
+    def __init__(self, seasons_data, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.seasons_data = seasons_data
+
+    def on_select_changed(self, event: Select.Changed):
+        episodes_scroll = self.query_one('#episodes-scroll')
+        episodes_scroll.remove_children()
+        episode_cards = []
+        season_id = event.value
+        for episode_data in self.seasons_data[season_id]:
+            episode_cards.append(EpisodeCard(season_id, episode_data))
+        episodes_scroll.mount_compose(episode_cards)
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            yield Button('Previous', disabled=True)
+            options = [(f'Season {number}', number) for number in range(1, len(self.seasons_data))]
+            options[-1] = ('Special', 0)
+            season_selector = Select(options)
+            yield season_selector
+            season_selector.value = options[0][1]
+            yield Button('Next')
+        yield VerticalScroll(id='episodes-scroll')
+
+
+class StreamSelector(VerticalScroll):
+    item_id = reactive(str)
+
+    class Submitted(Message):
+        def __init__(self, stream_data) -> None:
+            super().__init__()
+            self.stream_data = stream_data
+
+    def __init__(self, item_type, item_id=None, **kwargs):
+        super().__init__(**kwargs)
+        self.item_type = item_type
+        if item_id:
+            self.item_id = item_id
+
+    def watch_item_id(self, item_id):
+        self.remove_children()
+        self.streams = get_available_streams(item_id, self.item_type)
+        stream_buttons = [Button(stream['name'], id=str(index)) for index, stream in enumerate(self.streams)]
+        self.mount_all(stream_buttons)
+
+    def on_button_pressed(self, event: Button.Pressed):
+        stream_data = self.streams[int(event.button.id)]
+        self.post_message(self.Submitted(stream_data))
+
+
+class DetailsScreen(Screen):
+    def __init__(self, entry, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.metadata = get_metadata(entry)
+
+    def on_stream_selector_submitted(self, event: StreamSelector.Submitted):
+        with self.app.suspend():
+            start_download(self.app.torrent_session_handle, event.stream_data)
+
+    def compose(self) -> ComposeResult:
+        with Horizontal(id='content'):
+            with VerticalScroll(id='details'):
+                yield UrlImage(self.metadata['logo'], id='logo')
+                with Horizontal(classes='stats'):
+                    yield Label(self.metadata.get('runtime', ''))
+                    yield Label(self.metadata['year'])
+                    yield Label(self.metadata['imdbRating'])
+                with Horizontal(classes='stats'):
+                    for member in self.metadata['cast']:
+                        yield Label(member)
+                yield Label(self.metadata['description'], id='summary')
+            selectors_switcher = ContentSwitcher(id='selectors-switcher')
+            with selectors_switcher:
+                stream_selector = StreamSelector(self.metadata['type'])
+                yield stream_selector
+                if self.metadata['type'] == 'movie':
+                    stream_selector.item_id = self.metadata['imdb_id']
+                    selectors_switcher.current(stream_selector)
+                else:
+                    episode_selector = EpisodeSelector(self.metadata['seasons_data'])
+                    yield episode_selector
+                    selectors_switcher.current(episode_selector)
+
+
 class MainScreen(Screen):
     def compose(self) -> ComposeResult:
         with Center():
-            yield Input(id="Search")
-        yield PosterList(id="movies")
-        yield PosterList(id="series")
+            yield Input(id='Search')
+        yield PosterList(id='movies')
+        yield PosterList(id='series')
         yield Footer()
 
     def on_input_submitted(self, event: Input.Submitted) -> None:
         event.input.blur()
         entries = search_catalog(event.value)
-        self.query_one("#movies").update_posters(entries["movie"])
-        self.query_one("#series").update_posters(entries["series"])
+        for content_type, data in entries.items():
+            self.query_one(f'{content_type}').posters_data = data
 
     def on_poster_selected(self, event: Poster.Selected):
-        self.app.push_screen(EntryDetailsScreen(event.entry_data))
-
-
-class EntryDetailsScreen(Screen):
-    def __init__(self, entry, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.data = get_metadata(entry)
-
-    async def on_mount(self):
-        self.query_one("#logo").image = await fetch_url(self.data["logo"])
-
-    def on_button_pressed(self, event: Button.Pressed):
-        with self.app.suspend():
-            start_download(self.app.torrent_session_handle, event.button.data)
-
-    def compose(self) -> ComposeResult:
-        with Horizontal(id="content"):
-            with VerticalScroll(id="details"):
-                yield Image(id="logo")
-                with Horizontal(classes="stats"):
-                    yield Label(self.data.get("runtime", ''))
-                    yield Label(self.data["year"])
-                    yield Label(self.data["imdbRating"])
-                with Horizontal(classes="stats"):
-                    for member in self.data["cast"]:
-                        yield Label(member)
-                yield Label(self.data["description"], id="summary")
-            with VerticalScroll(id="streams"):
-                if self.data["type"] == "movie":
-                    for stream in get_available_streams(self.data):
-                        button = Button(stream["title"])
-                        button.data = stream
-                        yield button
+        self.app.push_screen(DetailsScreen(event.entry_data))
 
 
 class StremtuiApp(App):
     BINDINGS = [
-        ("h", "focus('left')", "Left"),
-        ("l", "focus('right')", "Right"),
-        ("k", "focus('up')", "Up"),
-        ("j", "focus('down')", "Down"),
+        ('h', 'focus_left', 'Left'),
+        ('l', 'focus_right', 'Right'),
+        ('k', 'focus_up', 'Up'),
+        ('j', 'focus_down', 'Down'),
     ]
-    CSS_PATH = "style.css"
+    CSS_PATH = 'style.css'
 
     def __init__(self):
         super().__init__()
@@ -141,6 +236,6 @@ class StremtuiApp(App):
         close_session(self.torrent_session_handle)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = StremtuiApp()
     app.run()
--- streaming.py
+++ streaming.py
@@ -1,25 +1,24 @@
-from time import sleep
-import subprocess
-from pathlib import Path
-from collections import defaultdict
 import datetime
+import subprocess
+from collections import defaultdict
+from pathlib import Path
+from time import sleep
+
 from curl_cffi import requests
 from curl_cffi.requests.exceptions import HTTPError
-from libtorrent import session, parse_magnet_uri, torrent_flags, bencode, bdecode
+from libtorrent import bdecode, bencode, parse_magnet_uri, session, torrent_flags
 
-CONTENT_TYPES = ["series", "movie"]
-CATALOG_PROVIDER_URL = "https://v3-cinemeta.strem.io"
-TRACKERSLIST_FETCH_URL = (
-    "https://cdn.jsdelivr.net/gh/ngosang/trackerslist@master/trackers_best.txt"
-)
-STREAM_PROVIDERS_URL = ("https://torrentio.strem.fun",)
+CONTENT_TYPES = ['series', 'movie']
+CATALOG_PROVIDER_URL = 'https://v3-cinemeta.strem.io'
+TRACKERSLIST_FETCH_URL = 'https://cdn.jsdelivr.net/gh/ngosang/trackerslist@master/trackers_best.txt'
+STREAM_PROVIDERS_URL = ('https://torrentio.strem.fun',)
 DHT_ROUTERS = (
-    "dht.libtorrent.org:25401",
-    "dht.transmissionbt.com:6881",
-    "router.bittorrent.com:6881",
-    "router.utorrent.com:6881",
-    "dht.aelitis.com:6881",
-    "router.bt.ouinet.work:6881",
+    'dht.libtorrent.org:25401',
+    'dht.transmissionbt.com:6881',
+    'router.bittorrent.com:6881',
+    'router.utorrent.com:6881',
+    'dht.aelitis.com:6881',
+    'router.bt.ouinet.work:6881',
 )
 BASE_FOLDER = Path(__file__).parent.resolve()
 
@@ -28,7 +27,7 @@ def get_bootstrap_trackers():
     today_date_str = str(datetime.date.today())
 
     try:
-        with open(BASE_FOLDER / "tracker_cache", "r+") as tracker_cache_file:
+        with open(BASE_FOLDER / 'tracker_cache', 'r+') as tracker_cache_file:
             cache_date = tracker_cache_file.readline().strip()
             cached_trackers = tracker_cache_file.read().splitlines()
             if cache_date == today_date_str:
@@ -42,10 +41,10 @@ def get_bootstrap_trackers():
                     return cached_trackers
                 else:
                     tracker_cache_file.seek(0)
-                    tracker_cache_file.write(today_date_str + "\n" + raw_trackers)
+                    tracker_cache_file.write(today_date_str + '\n' + raw_trackers)
                     return raw_trackers.split()
     except FileNotFoundError:
-        with open(BASE_FOLDER / "tracker_cache", "w") as tracker_cache_file:
+        with open(BASE_FOLDER / 'tracker_cache', 'w') as tracker_cache_file:
             try:
                 response = requests.get(TRACKERSLIST_FETCH_URL)
                 response.raise_for_status()
@@ -53,17 +52,17 @@ def get_bootstrap_trackers():
             except HTTPError:
                 return []
             else:
-                tracker_cache_file.write(today_date_str + "\n" + raw_trackers)
+                tracker_cache_file.write(today_date_str + '\n' + raw_trackers)
                 return raw_trackers.split()
 
 
 def get_session_handle():
-    settings = {"dht_bootstrap_nodes": ",".join(DHT_ROUTERS)}
+    settings = {'dht_bootstrap_nodes': ','.join(DHT_ROUTERS)}
 
     session_handle = session(settings)
     session_handle.bootstrap_trackers = get_bootstrap_trackers()
     try:
-        with open(BASE_FOLDER / "session.dat", "rb") as session_cache:
+        with open(BASE_FOLDER / 'session.dat', 'rb') as session_cache:
             session_handle.load_state(bdecode(session_cache.read()))
     except FileNotFoundError:
         pass
@@ -75,82 +74,69 @@ def search_catalog(query):
     catalog = {}
 
     for content_type in CONTENT_TYPES:
-        entries_in_type = requests.get(
-            f"{CATALOG_PROVIDER_URL}/catalog/{content_type}/top/search={query}.json"
-        ).json()["metas"]
+        entries_in_type = requests.get(f'{CATALOG_PROVIDER_URL}/catalog/{content_type}/top/search={query}.json').json()[
+            'metas'
+        ]
         catalog[content_type] = entries_in_type
 
     return catalog
 
 
 def get_metadata(entry):
-    metadata = requests.get(
-        f"{CATALOG_PROVIDER_URL}/meta/{entry['type']}/{entry['imdb_id']}.json"
-    ).json()["meta"]
+    metadata = requests.get(f'{CATALOG_PROVIDER_URL}/meta/{entry["type"]}/{entry["imdb_id"]}.json').json()['meta']
 
-    if entry["type"] == "series":
-        metadata["seasons"] = defaultdict(list)
-        for video in metadata["videos"]:
-            metadata["seasons"][video["season"]].append(video)
+    if entry['type'] == 'series':
+        seasons_data = []
+        for video in metadata['videos']:
+            season = video['season']
+            while len(seasons_data) < season + 1:
+                seasons_data.append([])
+
+            seasons_data[season].append(video)
+        metadata['seasons_data'] = seasons_data
 
     return metadata
 
 
-def get_available_streams(entry_data):
-    if entry_data["type"] == "series":
-        item_id = ":".join(
-            [
-                entry_data["imdb_id"],
-                entry_data.get("selected_season", ""),
-                entry_data.get("selected_episode", ""),
-            ]
-        )
-    else:
-        item_id = entry_data["imdb_id"]
-
+def get_available_streams(item_id, item_type):
     available_streams = []
     for stream_provider in STREAM_PROVIDERS_URL:
-        available_streams.extend(
-            requests.get(
-                f"{stream_provider}/stream/{entry_data['type']}/{item_id}.json"
-            ).json()["streams"]
-        )
+        available_streams.extend(requests.get(f'{stream_provider}/stream/{item_type}/{item_id}.json').json()['streams'])
 
     return available_streams
 
 
 def start_download(session_handle: session, stream_data):
-    magnet_link = f"magnet:?xt=urn:btih:{stream_data['infoHash']}"
+    magnet_link = f'magnet:?xt=urn:btih:{stream_data["infoHash"]}'
 
     current_torrent_trackers = session_handle.bootstrap_trackers.copy()
-    for source in stream_data.get("sources", []):
-        if source.startswith("tracker:"):
-            current_torrent_trackers.append(source.lstrip("tracker:"))
-        if not source.startswith("dht:"):
+    for source in stream_data.get('sources', []):
+        if source.startswith('tracker:'):
+            current_torrent_trackers.append(source.lstrip('tracker:'))
+        if not source.startswith('dht:'):
             current_torrent_trackers.append(source)
 
     params = parse_magnet_uri(magnet_link)
-    params.save_path = "."
+    params.save_path = '.'
     params.flags |= torrent_flags.sequential_download
     params.flags |= torrent_flags.upload_mode
     params.trackers = current_torrent_trackers
 
     torrent_handle = session_handle.add_torrent(params)
 
-    print("Downloading Metadata...")
+    print('Downloading Metadata...')
     while not torrent_handle.status().has_metadata:
         sleep(1)
-        print(f"Peers: {torrent_handle.status().num_peers}")
+        print(f'Peers: {torrent_handle.status().num_peers}')
 
     torrent_info = torrent_handle.torrent_file()
     num_files = torrent_info.num_files()
-    files = torrent_info.files()
     priorities = [0] * num_files
-    stream_file_index = stream_data["fileIdx"]
+    stream_file_index = stream_data['fileIdx']
     priorities[stream_file_index] = 1
-    stream_file_extension = Path(stream_data["behaviorHints"]["filename"]).suffix
+    stream_file_extension = Path(stream_data['behaviorHints']['filename']).suffix
 
-    stream_buffer_file = BASE_FOLDER / f"stream_buffer{stream_file_extension}"
+    stream_buffer_file = BASE_FOLDER / f'stream_buffer{stream_file_extension}'
     if stream_buffer_file.exists():
         stream_buffer_file.unlink()
 
@@ -162,12 +148,12 @@ def start_download(session_handle: session, stream_data):
         print(torrent_handle.status().total_download)
         sleep(1)
 
-    player_process = subprocess.Popen(["mpv", stream_buffer_file, "--keep-open"])
+    player_process = subprocess.Popen(['mpv', stream_buffer_file, '--keep-open'])
     player_process.wait()
     session_handle.remove_torrent(torrent_handle)
     stream_buffer_file.unlink()
 
 
 def close_session(session_handle: session):
-    with open("session.dat", "wb") as session_state_savefile:
+    with open('session.dat', 'wb') as session_state_savefile:
         session_state_savefile.write(bencode(session_handle.save_state()))
