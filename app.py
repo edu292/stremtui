1fe301ce34ffc0b0ed63f2d658f5791bac49a460
edu292 <eduskoroboatei@gmail.com>
2025-12-20 10:21:12 -0400

    Episode Selector Implementation / style overhaul

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ from textual.message import Message
 from textual.reactive import reactive
 from textual.screen import Screen
 from textual.widgets import Button, ContentSwitcher, Footer, Input, Label, Select
+from textual_image.renderable import Image as AutoRenderable
 from textual_image.widget import Image
+from PIL import Image as PILImage
 
 from streaming import (
     close_session,
@@ -27,13 +29,13 @@ from streaming import (
 
 async def fetch_url(url):
     async with AsyncClient() as client:
-        response = await client.get(url)
+        response = await client.get(url, follow_redirects=True)
         if not response.is_success:
             return
         return BytesIO(response.content)
 
 
-class UrlImage(Image):
+class UrlImage(Image, Renderable=AutoRenderable):
     def __init__(self, url, **kwargs):
         super().__init__(image=None, **kwargs)
         self.url = url
@@ -42,7 +44,6 @@ class UrlImage(Image):
         response = await fetch_url(self.url)
         if not response:
             return
-
         self.image = response
 
 
@@ -94,29 +95,27 @@ class PosterList(HorizontalScroll):
 class EpisodeCard(Horizontal):
     can_focus = True
 
-    class Submitted(Message):
-        def __init__(self, season_id, episode_id) -> None:
+    class Selected(Message):
+        def __init__(self, episode_id) -> None:
             super().__init__()
+            self.episode_id = episode_id
 
-    def __init__(self, season_id, episode_data, **kwargs):
+    def __init__(self, episode_data, **kwargs):
         super().__init__(**kwargs)
-        self.season_id = season_id
         self.episode_data = episode_data
 
     def on_click(self):
-        self.post_message(self.Submitted(self.season_id, self.episode_id))
+        self.post_message(self.Selected(self.episode_data['id']))
 
     def compose(self) -> ComposeResult:
         yield UrlImage(self.episode_data['thumbnail'])
-        with Horizontal(classes='episode-details'):
+        with Vertical(classes='episode-details'):
             yield Label(self.episode_data['name'], classes='episode-name')
             yield Label(self.episode_data['released'], classes='episode-release')
 
 
 class EpisodeSelector(Vertical):
-    def __init__(self, seasons_data, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.seasons_data = seasons_data
+    seasons_data = reactive([])
 
     def on_select_changed(self, event: Select.Changed):
         episodes_scroll = self.query_one('#episodes-scroll')
@@ -124,47 +123,91 @@ class EpisodeSelector(Vertical):
         episode_cards = []
         season_id = event.value
         for episode_data in self.seasons_data[season_id]:
-            episode_cards.append(EpisodeCard(season_id, episode_data))
-        episodes_scroll.mount_compose(episode_cards)
+            episode_cards.append(EpisodeCard(episode_data))
+        episodes_scroll.mount_all(episode_cards)
+        episodes_scroll.scroll_home()
+
+    def watch_seasons_data(self, seasons_data):
+        if not seasons_data:
+            return
+        options = [(f'Season {number}', number) for number in range(1, len(seasons_data))]
+        if seasons_data[0]:
+            options.append(('Special', 0))
+        seasons_select = self.query_one('#seasons-select')
+        seasons_select.set_options(options)
+        seasons_select.allow_black = False
+        seasons_select.value = 1
 
     def compose(self) -> ComposeResult:
-        with Horizontal():
+        with Horizontal(id='controls'):
             yield Button('Previous', disabled=True)
-            options = [(f'Season {number}', number) for number in range(1, len(self.seasons_data))]
-            options[-1] = ('Special', 0)
-            season_selector = Select(options)
-            yield season_selector
-            season_selector.value = options[0][1]
+            yield Select([], id='seasons-select')
             yield Button('Next')
         yield VerticalScroll(id='episodes-scroll')
 
 
 class StreamSelector(VerticalScroll):
-    item_id = reactive(str)
+    item_id = reactive('', init=False)
 
     class Submitted(Message):
         def __init__(self, stream_data) -> None:
             super().__init__()
             self.stream_data = stream_data
 
-    def __init__(self, item_type, item_id=None, **kwargs):
+    def __init__(self, item_type, **kwargs):
         super().__init__(**kwargs)
         self.item_type = item_type
-        if item_id:
-            self.item_id = item_id
 
-    def watch_item_id(self, item_id):
-        self.remove_children()
+    async def watch_item_id(self, item_id):
+        await self.remove_children()
         self.streams = get_available_streams(item_id, self.item_type)
-        stream_buttons = [Button(stream['name'], id=str(index)) for index, stream in enumerate(self.streams)]
-        self.mount_all(stream_buttons)
+        stream_buttons = [Button(stream['title'], id=f'stream-{index}') for index, stream in enumerate(self.streams)]
+        await self.mount_all(stream_buttons)
 
     def on_button_pressed(self, event: Button.Pressed):
-        stream_data = self.streams[int(event.button.id)]
+        stream_data = self.streams[int(event.button.id.lstrip('stream-'))]
         self.post_message(self.Submitted(stream_data))
+
+    def compose(self) -> ComposeResult:
+        return super().compose()
+
+
+class SelectionManager(ContentSwitcher):
+    BINDINGS = [('b', 'back')]
+
+    def __init__(self, entry_type, entry_id, seasons_data=None, **kwargs):
+        initial_tab = 'stream-selector' if entry_type == 'movie' else 'episode-selector'
+        super().__init__(initial=initial_tab, **kwargs)
+        self.entry_type = entry_type
+        self.entry_id = entry_id
+        self.seasons_data = seasons_data
+
+    def on_mount(self):
+        if self.entry_type == 'movie':
+            self.query_one('#stream-selector').item_id = self.entry_id
+            self.current = 'stream-selector'
+        else:
+            self.query_one('#episode-selector').seasons_data = self.seasons_data
+            self.current = 'episode-selector'
+
+    def on_episode_card_selected(self, event: EpisodeCard.Selected):
+        self.query_one('#stream-selector').item_id = event.episode_id
+        self.current = 'stream-selector'
+
+    def action_back(self):
+        if self.entry_type == 'series' and self.current == 'stream-selector':
+            self.current = 'episode-selector'
+        else:
+            self.screen.action_back()
+
+    def compose(self) -> ComposeResult:
+        yield EpisodeSelector(id='episode-selector')
+        yield StreamSelector(self.entry_type, id='stream-selector')
 
 
 class DetailsScreen(Screen):
+    BINDINGS = [('b', 'back')]
+
     def __init__(self, entry, **kwargs) -> None:
         super().__init__(**kwargs)
         self.metadata = get_metadata(entry)
@@ -172,6 +215,9 @@ class DetailsScreen(Screen):
     def on_stream_selector_submitted(self, event: StreamSelector.Submitted):
         with self.app.suspend():
             start_download(self.app.torrent_session_handle, event.stream_data)
+
+    def action_back(self):
+        self.app.pop_screen()
 
     def compose(self) -> ComposeResult:
         with Horizontal(id='content'):
@@ -185,32 +231,21 @@ class DetailsScreen(Screen):
                     for member in self.metadata['cast']:
                         yield Label(member)
                 yield Label(self.metadata['description'], id='summary')
-            selectors_switcher = ContentSwitcher(id='selectors-switcher')
-            with selectors_switcher:
-                stream_selector = StreamSelector(self.metadata['type'])
-                yield stream_selector
-                if self.metadata['type'] == 'movie':
-                    stream_selector.item_id = self.metadata['imdb_id']
-                    selectors_switcher.current(stream_selector)
-                else:
-                    episode_selector = EpisodeSelector(self.metadata['seasons_data'])
-                    yield episode_selector
-                    selectors_switcher.current(episode_selector)
+            yield SelectionManager(self.metadata['type'], self.metadata['imdb_id'], self.metadata.get('seasons_data'))
 
 
 class MainScreen(Screen):
     def compose(self) -> ComposeResult:
         with Center():
             yield Input(id='Search')
-        yield PosterList(id='movies')
-        yield PosterList(id='series')
-        yield Footer()
+        yield PosterList(id='movie-posters')
+        yield PosterList(id='series-posters')
 
     def on_input_submitted(self, event: Input.Submitted) -> None:
         event.input.blur()
         entries = search_catalog(event.value)
         for content_type, data in entries.items():
-            self.query_one(f'{content_type}').posters_data = data
+            self.query_one(f'#{content_type}-posters').posters_data = data
 
     def on_poster_selected(self, event: Poster.Selected):
         self.app.push_screen(DetailsScreen(event.entry_data))
